c66532a10d43848387b8beff81fb09cc370b0ebb
FM Dube <31796906+fransdube@users.noreply.github.com>
2026-04-01 15:16:53 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Enable globalization invariant mode to avoid missing libicu dependencies on barebones linux
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+
 # Define the .NET version we want
 DOTNET_VERSION=10.0.103
 
